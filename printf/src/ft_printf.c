cd141004bd8f5aa8668d91ed1fff3fd2427ae309
Huizhi Lin <hlin@f0r6s7.codam.nl>
2019-12-12 18:05:03 +0100

    create format parse with variandic functions

--- printf/src/ft_printf.c
+++ printf/src/ft_printf.c
@@ -11,8 +11,45 @@
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+#include <stdio.h>
 
 int		ft_printf(const char *format, ...)
 {
-	t_pf	*lst
+	va_list	arg;
+	va_list	arg_cp;
+	char	*str;
+	int		len;
+	int		arg_count;
+
+	len = 0;
+	arg_count = 0;
+	va_start(arg, format);
+	va_copy(arg_cp, arg);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			str = va_arg(arg, char*);
+			printf("argument is: %s\n", str);
+			arg_count++;
+		}
+		format++;
+		len++;
+	}
+	printf("arguments count: %d\n", arg_count);
+	while (arg_count > 0)
+	{
+		printf("argument copy is: %s\n", va_arg(arg_cp, char*));
+		arg_count--;
+	}
+	va_end(arg);
+	return (len);
+}
+
+int		main(void)
+{
+	int		count;
+
+	count = ft_printf("4 arg: %s %s %s", "This", "is", "Test");
+	printf("return value: %d\n", count);
 }
