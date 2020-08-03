9c58ca8055e30da1153ced3f9fde508c5aff76f5
Hz-Lin <huizhi.lin0@gmail.com>
2020-08-03 12:38:02 +0200

    finally test

--- ft_server/srcs/autoindex.sh
+++ ft_server/srcs/autoindex.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $1 == "off" ]
 then
-	sed -i '/autoindex/c autoindex off;' /nginx/sites-available/catanmap
+	sed -i '/autoindex/c autoindex off;' /etc//nginx/sites-available/server.conf
 elif [ $1 == "on" ]
 then
-	sed -i '/autoindex/c autoindex on;' /nginx/sites-available/catanmap
+	sed -i '/autoindex/c autoindex on;' /etc//nginx/sites-available/server.conf
 else
 	echo "Incorrect use of script, use on or off"
 fi
